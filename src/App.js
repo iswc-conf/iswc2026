51e9d1fed9fd6c63854afc9ef7b701e1a8a6b914
Ivan Diliso <33879857+ivandiliso@users.noreply.github.com>
2025-10-27 11:40:06 +0100

    Message Update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
     <>
         <div className="flex justify-center items-center mt-10 pt-10">
       <div className="bg-white shadow-lg rounded-2xl px-8 py-6 text-center border border-gray-200 max-w-lg">
-        <h1 className="font-bold text-5xl mb-10">ISWC 2026</h1>
+        <h1 className="font-bold text-5xl mb-10">ISWC 2026 Coming Soon</h1>
         <p className="text-xl font-semibold text-gray-800 mb-2">
           The website will be updated soon
         </p>
