b9c40b610e28847c82282e34da471292223f74e4
ivandiliso <ivandiliso@users.noreply.github.com>
2026-07-29 15:46:10 +0000

    deploy: a76b053327b32f169e1e891624f94a1ac8fd6981

--- assets/BaseContainer-CFnclQXY.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CGZ9mYCs.js";import{P as t}from"./Page-tMbQ5zxk.js";const m=({children:o,...s})=>r.jsx(t,{...s,children:o});export{m as B};
--- assets/Card-Ba5kTCIF.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CGZ9mYCs.js";function i({children:s,className:a=""}){return r.jsx("div",{className:`card ${a}`.trim(),children:r.jsx("div",{className:"card-body",children:s})})}export{i as C};
