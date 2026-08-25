2584be33473ea4e3e8994058f8c39a19c3072c66
Ivan Diliso <33879857+ivandiliso@users.noreply.github.com>
2026-08-25 18:30:35 +0200

    dagstul workshops

--- src/components/program/Dagstuhl.jsx
+++ src/components/program/Dagstuhl.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { useState, useRef } from "react";
 import BaseContainer from "../general/BaseContainer";
 import Header from "../general/Header";
-import { dagshtul } from "./data/dagshtul";
 import DescriptiveTable from "../general/DescriptiveTable";
 import DeclareSoon from "../general/declareSoon";
+import { dags } from "./data/dagshtul";
 
 export const DagstuhlWorkshops = () => {
 
@@ -12,8 +12,9 @@ export const DagstuhlWorkshops = () => {
     return (
         <BaseContainer width="wide">
         <Header> Accepted Dagstuhl-style Workshops</Header>
-        <DeclareSoon></DeclareSoon>
-        {/** <DescriptiveTable data={dagshtul} title={"Workshop Title"} description={"Organizers"}></DescriptiveTable>}*/}
+                <div className="text-center">
+                <p className="pt-2 fw-semibold">Click on a workshop title below to open its full description.</p></div>
+                <DescriptiveTable title={"Dagstuhl-style Workshops Title"}  data={dags} header_title={"Abstract"} unit="Dagstuhl-style Workshops"></DescriptiveTable>
         </BaseContainer>
     );
 };
