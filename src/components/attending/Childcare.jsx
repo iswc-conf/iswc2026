ccf8016eb867210c7ec9bf774b1674432c7105f3
Ivan Diliso <33879857+ivandiliso@users.noreply.github.com>
2026-08-25 15:16:45 +0200

    Childcare Update

--- src/components/attending/Childcare.jsx
+++ src/components/attending/Childcare.jsx
@@ -1,17 +1,20 @@
 import BaseContainer from '../general/BaseContainer'
-import DeclareSoon from '../general/declareSoon'
 import Header from '../general/Header'
-import SubHeader from '../general/SubHeader'
-import UnderlineHeader from '../general/UnderlineHeader'
 import ExternalLink from '../general/ExternalLink'
-import { theme } from "../../theme";
-
 
 export const Childcare = () => {
     return (
         <BaseContainer>
             <Header>Childcare</Header>
-            <p>If you are planning to travel to ISWC with your kids and need to arrange childcare, the following providers offer services in English:</p>
+
+            <p className="iswc-callout">
+                Below are some useful links you may use to arrange childcare during your
+                stay in Bari. Please note that childcare is <b>not</b> a service offered by
+                the ISWC conference; these are independent providers, listed here for your
+                convenience.
+            </p>
+
+            <p>The following providers offer babysitting services in English:</p>
 
             <ul>
                 <li><ExternalLink href="https://www.sitly.it/en/babysitter/bari">https://www.sitly.it/en/babysitter/bari</ExternalLink></li>
@@ -19,10 +22,15 @@ export const Childcare = () => {
                 <li><ExternalLink href="https://www.cronoshare.it/servizi/babysitter-inglese/bari/bari">https://www.cronoshare.it/servizi/babysitter-inglese/bari/bari</ExternalLink></li>
             </ul>
 
-            <p>Please reach out to them directly to confirm their availability, rates, and services, and to make any necessary reservations in advance.</p>
+            <p>
+                Please reach out to them directly to confirm their availability, rates and
+                services, and to make any necessary reservations in advance.
+            </p>
 
-             <p className="iswc-callout">Please note that childcare is not provided by the ISWC conference itself; these are independent providers. Nonetheless, do not hesitate to contact us if you require assistance</p>
-    
+            <p>
+                For further information, you can contact us at{" "}
+                <ExternalLink href="mailto:iswc2026@cicsud.it">iswc2026@cicsud.it</ExternalLink>.
+            </p>
         </BaseContainer>
     )
 }
