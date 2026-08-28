9968b47592fbee73bbf26c5c2797f144a1140783
ivandiliso <ivandiliso@users.noreply.github.com>
2026-08-28 20:46:45 +0000

    deploy: 1bd44c6cbe4d3496d8485591a36846726790ef4c

--- assets/BaseContainer-BkZHnq98.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-Ca4mvTt1.js";import{P as t}from"./Page-B4L_kxW1.js";const m=({children:o,...s})=>r.jsx(t,{...s,children:o});export{m as B};
---
+++ assets/SubHeader-BJiRDglu.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-Cc6p0_yK.js";const t=({children:s,className:a=""})=>e.jsx("h3",{className:`iswc-subheading ${a}`.trim(),children:s});export{t as S};
