24ef85643d06afd859f802804154da025b09aeba
ivandiliso <ivandiliso@users.noreply.github.com>
2026-09-17 08:10:47 +0000

    deploy: 199a8c8305b2d0991c33195929fc4d84645b3c90

---
+++ assets/BaseContainer-Clh_-GNT.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-B7nbuMr1.js";import{P as t}from"./Page-4u87TU6f.js";const m=({children:o,...s})=>r.jsx(t,{...s,children:o});export{m as B};
---
+++ assets/DescriptiveTable-C4wEL6nS.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-B7nbuMr1.js";import{D as i}from"./DataTable-DK-45CNN.js";const b=({title:e,description:a,data:s,header_title:r,unit:t})=>o.jsx(i,{data:s,titleLabel:e,secondaryLabel:a,secondaryField:"organizers",detailLabel:r,showLinks:!0,unit:t});export{b as D};
