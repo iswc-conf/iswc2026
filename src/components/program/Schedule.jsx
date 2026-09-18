6c6c8a35892a80c0405f30ed016f9ca69cb0f621
Ivan Diliso <33879857+ivandiliso@users.noreply.github.com>
2026-09-18 09:42:32 +0200

    Ammend

--- src/components/program/Schedule.jsx
+++ src/components/program/Schedule.jsx
@@ -205,7 +205,11 @@ export const Schedule = () => {
       <Header>Schedule</Header>
 
 
-      
+            <p className="iswc-callout">
+            This is a draft schedule and may still change.  Room assignment can be subject to changes. All times are local (CEST).
+ 
+            </p>
+
 
       {/* Day switcher */}
       <div className="iswc-schedule__days" role="group" aria-label="Select a day">
