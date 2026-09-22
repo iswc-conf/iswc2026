3bd7a6da42c24322b497bea5ec5fc4d8a77812b3
ivandiliso <ivandiliso@users.noreply.github.com>
2026-09-22 09:29:45 +0000

    deploy: 2dba0bbb1f2815aa29b490f7c26757b5dc225fa2

--- assets/BaseContainer-B2wSwL80.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-DsGrECfy.js";import{P as t}from"./Page-CTSJ6RPD.js";const m=({children:o,...s})=>r.jsx(t,{...s,children:o});export{m as B};
---
+++ assets/BaseContainer-OSsDpa3c.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BG6chik5.js";import{P as t}from"./Page-CUFgA-ME.js";const m=({children:o,...s})=>r.jsx(t,{...s,children:o});export{m as B};
